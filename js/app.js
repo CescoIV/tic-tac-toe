1d92a48ff4f26c16fee94a0e679f7a25143e576b
CescoIV <crordaz@gmail.com>
2017-09-23 21:19:53 -0500

    horizontal logic works, revert to if remove experiment does nto work

--- js/app.js
+++ js/app.js
@@ -1,23 +1,24 @@
 // var a = [...document.querySelectorAll('.row')];
 
 
-var x = "url('images/swords.png')"
-var o = "url('images/cannonballs.png')"
+var x = 'url("images/swords.png")';
+var o = 'url("images/cannonballs.png")';
 var player = x;
+
 function start(){
 	var rows = [...document.querySelectorAll('.row')];
 	
-	rows.forEach((row)=>{
-		row.addEventListener('click',(el)=>{
-			//if click on row AND div inside row
-			if(el.target && el.target.nodeName == 'DIV'){
-				setPlayer(el.target,el);
+	rows.forEach((el)=>{
+		el.addEventListener('click',function onClick(event){
+			//if click on row AND div inside row, block gets executed
+			if(event.target && event.target.nodeName == 'DIV'){
+				setPlayer(event.target,event);
 			}
 		})
 	})
 }
 
-function setPlayer(box, event){
+function setPlayer(box, event, funct){
 	var msg = document.querySelector('h3');
 
 	//if box has been marked, raise alert
@@ -28,15 +29,18 @@ function setPlayer(box, event){
 		//Make the box background the player's img
 		//check who player is, then switch to 'o' or 'x' player(pass the turn)
 		box.style.backgroundImage = player;
+		if(hasWon(player)){
+			event.target.removeEventListener('click', funct);
+			console.log('ayylmao');
+		}
 		player === x ? player = o : player = x;
 		setText(player);
 		msg.innerText = '';	
 	}
 }
 
-function setText(player){
+function setText(player, won = false){
 	var msg = document.querySelector('h2');
-
 	if(player === x){
 		msg.innerText = "Swords up Lads!";
 	}else{
@@ -44,3 +48,24 @@ function setText(player){
 	}
 }
 
+function hasWon(player){
+	var boxes = [...document.querySelectorAll('.box')];
+
+	for(var i=0;i<3;i++){
+		var horizontal = checkHor(i,boxes,player);
+		if(horizontal){
+			return true;
+		}	
+	}
+}
+
+function checkHor(index,boxes,player){
+	var first = boxes[index].style.backgroundImage;
+	var second = boxes[index+3].style.backgroundImage;
+	var third = boxes[index+6].style.backgroundImage;
+
+	if(first === player && second === player && third === player){
+		return true;
+	}
+	return false;
+}
